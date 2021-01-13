bfde6ca1d475510e1f38d009256c93f9e0ef918e
QianqiJia <s1945609@ed.ac.uk>
2021-01-13 12:12:11 +0000

    Added my birthday

--- hello.py
+++ hello.py
@@ -1,4 +1,5 @@
-name = 'Josephine'
-age = 36
+name = 'QianqiJia'
+age = 19
+birthday = '18th February'
 
-print(f'Hi, my name is {name}, and I am {age} years old.')
+print(f'Hi, my name is {name}, I am {age} years old, and my birthday is {birthday}!')
